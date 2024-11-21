998845ffbdbf8e8a3101f1fbe855bc787bc39ec6
Csanad <you@example.com>
2024-11-21 09:04:52 +0100

    Add project files.

---
+++ ForumMotor/Models/User.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace ForumMotor.Models
+{
+    public class User : IdentityUser
+    {
+        public string? LastName { get; set; }
+        public string? FirstName { get; set; }
+        public DateTime? BirthDate { get; set; }
+    }
+}
